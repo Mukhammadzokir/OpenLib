3438dbfd89b604531166d2dbab9d8028be31cedf
Mukhammadzokir <139150511+MuxammadZokir@users.noreply.github.com>
2024-02-13 08:11:33 +0500

    Create all entites and DbContext

---
+++ src/OpenLibrary.Domain/Entities/BookAuthor.cs
@@ -0,0 +1,11 @@
+﻿using OpenLibrary.Domain.Commons;
+
+namespace OpenLibrary.Domain.Entities;
+
+public class BookAuthor : Auditable
+{
+    public long BookId { get; set; }
+    public Book Book { get; set; }
+    public long AuthorId { get; set; }
+    public Author Author { get; set; }
+}
---
+++ src/OpenLibrary.Domain/Entities/Publisher.cs
@@ -0,0 +1,13 @@
+﻿using OpenLibrary.Domain.Commons;
+
+namespace OpenLibrary.Domain.Entities;
+
+public class Publisher : Auditable
+{
+    public string Name { get; set; }
+    public string Address { get; set; }
+    public string PhoneNumber { get; set; }
+    public string Email { get; set; }
+    public ICollection<Book> Books { get; set; }
+}
+
